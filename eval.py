5d94daef43d2d6dc76ecab962a2bf358a0f20394
municola <municola@student.ethz.ch>
2021-12-27 16:50:29 +0100

    fixed eval file and added evaluation results

--- eval.py
+++ eval.py
@@ -30,7 +30,7 @@ def main():
 
     # Load Model and data
     model, _, _, _ = load_model(model_name, config, device)
-    test_dataloader = load_data(test_path, batch_size)
+    test_dataloader = load_data(test_path, batch_size, model_name)
     print(f"Evaluating on adversarial test set: {test_adv_bool}")
 
     # Evaluate
